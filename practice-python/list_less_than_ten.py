eba04784c29933f43306a0033fd5ed11e851bb56
martazaryn <martazaryn@gmail.com>
2017-04-25 18:16:41 +0200

    List less than ten exercise (although it's 5 in description :) )

---
+++ practice-python/list_less_than_ten.py
@@ -0,0 +1,23 @@
+# Take a list, say for example this one:
+
+#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5.
+
+# Extras:
+
+# Instead of printing the elements one by one, make a new list that has all the elements 
+# less than 5 from this list in it and print out this new list.
+# Ask the user for a number and return a list that contains only elements from 
+# the original list a that are smaller than that number given by the user.
+
+l = [-4, 2, 3, 5, 7]
+
+def list_less(l):
+	results = []
+	for n in l:
+		if n < 5:
+			results.append(n)
+	# return results
+	print(results)
+
+list_less(l)
