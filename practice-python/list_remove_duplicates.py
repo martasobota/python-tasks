6618e7536dba5298554310cd9b73f9847f4cd37a
martazaryn <martazaryn@gmail.com>
2017-05-14 20:45:08 +0200

    List remove duplicates practice python task

---
+++ practice-python/list_remove_duplicates.py
@@ -0,0 +1,18 @@
+A = [1, 2, 2, 4, 6, 6, 8, 8, 8, 1, 1, 2]
+
+# solution 1
+
+def solution_1(A):
+	B = []
+	for i in A:
+		if i not in B:
+			B.append(i)
+	return B
+
+
+# solution 2
+def solution_2(A):
+	return set(A)
+
+print solution_1(A)
+print solution_2(A)
