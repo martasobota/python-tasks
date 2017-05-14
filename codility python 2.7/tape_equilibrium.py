b518a8bc1a79bb7befc9c4ba88f7a74166397048
martazaryn <martazaryn@gmail.com>
2017-05-14 12:38:41 +0200

    Type equilibrium codility task

---
+++ codility python 2.7/tape_equilibrium.py
@@ -0,0 +1,12 @@
+def solution(A):
+    front = A[0]
+    end = sum(A[1:])
+    min_diff = abs(end-front)
+    
+    for i in range(1, len(A)-1):
+        front += A[i]
+        end -= A[i]
+        if abs(front-end) < min_diff:
+            min_diff = abs(front-end)
+    
+    return min_diff
