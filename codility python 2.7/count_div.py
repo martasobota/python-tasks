3d757361656738aaa9160c8aed30140144ca43ee
martazaryn <martazaryn@gmail.com>
2017-05-14 20:36:03 +0200

    count_div.py codility task

---
+++ codility python 2.7/count_div.py
@@ -0,0 +1,13 @@
+A = 6
+B = 11
+K = 2
+
+def solution(A, B, K):
+    result = 0
+    for i in range(A, B):
+        if i % K == 0:
+            result += 1
+    return result
+
+
+print solution(A, B, K)
