95108dc37310be91cf9c3f6f7d3bd3ea42db0cdd
martazaryn <martazaryn@gmail.com>
2017-05-15 10:21:41 +0200

    Passing cars codility task

---
+++ codility python 2.7/passing_cars.py
@@ -0,0 +1,16 @@
+def solution(A):
+
+    length = len(A)
+
+    east = 0
+    passing = 0
+    for i in range(length):
+        if A[i] == 0:
+            east += 1
+        else:
+            passing += east
+
+            if passing > 1000000000:
+                return -1
+
+    return passing
