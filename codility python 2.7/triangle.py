5c4b3df8bdbed1f0891eba6d6abec157fcb38bbb
martazaryn <martazaryn@gmail.com>
2017-05-16 08:48:49 +0200

    Triangle codility task

---
+++ codility python 2.7/triangle.py
@@ -0,0 +1,11 @@
+def solution(A):
+    
+    length = len(A)
+    if length < 3:
+        return 0
+    
+    A.sort()
+    for i in xrange(length-2):
+        if A[i]+A[i+1] > A[i+2]:
+            return 1
+    return 0
