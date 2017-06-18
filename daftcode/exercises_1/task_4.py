d31f8c05c86bc265cfc4cab58abf403133989588
martazaryn <martazaryn@gmail.com>
2017-06-18 16:08:41 +0200

    Task 4 - new week dict

---
+++ daftcode/exercises_1/task_4.py
@@ -0,0 +1,2 @@
+# Powerset - Napisać kod tworzęcy ze zbioru A = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9} zbiór zawierający wszystkie podzbiory A 
+# (włącznie z pustym i A). UWAGA: w python zbiory nie mogą być elementami innych zbiorów, proszę użyć tupli jako zbiorów wewnętrznych
