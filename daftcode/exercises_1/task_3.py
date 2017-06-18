f3a8ed95a3570c0b84eed6ea5ed12435b550acca
martazaryn <martazaryn@gmail.com>
2017-06-18 18:21:29 +0200

    Task 3 week days completed

--- daftcode/exercises_1/task_3.py
+++ daftcode/exercises_1/task_3.py
@@ -3,7 +3,12 @@
 # do postaci: 
 # { 'Poniedziałek': 1, 'Środa': 3, 'Piątek': 5, 'Niedziela': 7, } Zamiana klucza z wartością i zostawienie tylko dni nieparzystych
 
-dict = { 1: 'Poniedziałek', 2: 'Wtorek', 3: 'Środa', 4: 'Czwartek', 5: 'Piątek', 6: 'Sobota', 7: 'Niedziela', } 
+week_dict = { 1: 'Poniedziałek', 2: 'Wtorek', 3: 'Środa', 4: 'Czwartek', 5: 'Piątek', 6: 'Sobota', 7: 'Niedziela', } 
 
-for key in dict:
-    print(key)
+new_week_dict = {}
+
+for key, value in week_dict.items():
+	if key % 2 != 0:
+		new_week_dict[value] = key
+
+print(new_week_dict)
