23812db71948c14459cb59266046eff3c594c07c
martazaryn <martazaryn@gmail.com>
2017-06-20 06:10:15 +0200

    Task 4 - powerset

--- daftcode/exercises_1/task_4.py
+++ daftcode/exercises_1/task_4.py
@@ -1,2 +1,13 @@
-# Powerset - Napisać kod tworzęcy ze zbioru A = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9} zbiór zawierający wszystkie podzbiory A 
+# Powerset - Napisać kod tworzący ze zbioru A = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9} zbiór zawierający wszystkie podzbiory A 
 # (włącznie z pustym i A). UWAGA: w python zbiory nie mogą być elementami innych zbiorów, proszę użyć tupli jako zbiorów wewnętrznych
+
+from itertools import chain, combinations
+
+A = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+def powerset(iterable):
+    s = list(iterable)
+    result = chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    return list(result)
+
+print(powerset(A))
