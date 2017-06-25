cde71ca46d54fcee654f7fa3ec58ca461be94a56
martazaryn <martazaryn@gmail.com>
2017-06-25 11:33:34 +0200

    DaftCode exercises 3, task 3 - Collatz generator

---
+++ daftcode/exercises_3/task_3_collatz.py
@@ -0,0 +1,19 @@
+def collatz_gen(x):
+    yield x
+    while x != 1:
+        if x % 2 == 0:
+            x /= 2
+            print(int(x))
+        else:
+            x = (x * 3) +1
+            print(int(x))
+        yield x
+    raise StopIteration
+
+# print(collatz_gen(1234))
+
+assert list(collatz_gen(1)) == [1]
+assert list(collatz_gen(2)) == [2, 1]
+assert list(collatz_gen(3)) == [3, 10, 5, 16, 8, 4, 2, 1]
+assert list(collatz_gen(12)) == [12, 6, 3, 10, 5, 16, 8, 4, 2, 1]
+assert list(collatz_gen(19)) == [19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
