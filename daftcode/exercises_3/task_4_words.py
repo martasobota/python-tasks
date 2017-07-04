9340c5ef24c64a9bf08b9a8a525c2c6f5555cd0d
martazaryn <martazaryn@gmail.com>
2017-07-04 18:28:00 +0200

    Words task

--- daftcode/exercises_3/task_4_words.py
+++ daftcode/exercises_3/task_4_words.py
@@ -1,10 +1,43 @@
-def get_words(in_file_name, out_file_name):
+def get_words_from_line(line):
+    data = set()
+    first = line.split('/')[-1]
+    second = first.split('.')[0]
+    word_start = 0
+    word_end = 0
+    checker = True
+
+    for seq in range(len(second)):
+        if second[seq].isalpha():
+            if checker:
+                checker = False
+                word_start = seq
+                # print('Word start: {}'.format(word_start))
+        else:
+            if not checker:
+                checker = True
+                word_end = seq
+                # print('Word end: {}'.format(word_end))
+                if word_start != word_end:
+                    data.add(second[word_start:word_end])
+    if not checker:
+        data.add(second[word_start:])
+    return data
+
+def get_words(in_file_name):
     words = set()
-    with open(file_name, 'r') as f:
+    save_words = set()
+    print('Creating list of words...')
+    with open(in_file_name, 'r') as f:
         for line in f:
-            words_in_lines = get_words_from_line(line)
-            words |= line_words
+            save_words = get_words_from_line(line)
+            words |= save_words
+    words = list(words)
+    print('Sorting words...')
+    words.sort()
+    print('Words sorted!')
+    return words
 
 with open('words_result.txt', 'w') as f:
     words = get_words('html.lst')
     f.writelines(w+'\n' for w in words)
+    print("Your file is ready!")
