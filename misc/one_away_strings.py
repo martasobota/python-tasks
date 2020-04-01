1400509a940642ad92692b30d976744fd3a09268
Marta Sobota <martazaryn@gmail.com>
2020-04-01 21:14:32 +0200

    WIP one away strings

--- misc/one_away_strings.py
+++ misc/one_away_strings.py
@@ -9,28 +9,55 @@ def create_table(s):
     return table
 
 def is_one_away(s1, s2):
-    result = 0
-    check_1 = {}
-    check_2 = {}
-    for item in s1:
-        if item in check_1:
-            check_1[item] += 1
-        else:
-            check_1[item] = 1
     if abs(len(s1) - len(s2)) > 1:
         return False
-    if result > 1:
-        return False
-    f
+    if len(s1) == len(s2):
+        index_1 = 0
+        index_2 = 0
+        result = 0
+        for i in range(len(s1)-1):
+            if s1[i] != s2[i]:
+                result += 1
+                if result > 1:
+                    return False
 
-# NOTE: The following input values will be used for testing your solution.
+    # if abs(len(s1) - len(s2)) == 1:
+    #     pass
+    return True
+    # result = 0
+    # check_1 = create_table(s1)
+    # check_2 = create_table(s2)
+    # for item in s1:
+    #     if item in check_2:
+    #         if check_1[item] != check_2[item]:
+    #             result += 1
+    #             # print(check_1[item] == check_2[item])
+    #             # print(check_1[item], " porównuję do", check_2[item])
+    #             # print("result ", result)
+    #     if result > 1:
+    #         return False
+    # return True
+
+
 is_one_away("abcde", "abcd")  # should return True
 is_one_away("abde", "abcde")  # should return True
 is_one_away("a", "a")  # should return True
 is_one_away("abcdef", "abqdef")  # should return True
 is_one_away("abcdef", "abccef")  # should return True
 is_one_away("abcdef", "abcde")  # should return True
-is_one_away("aaa", "abc")  # should return False
+print(is_one_away("aaa", "abc"))  # should return False
 is_one_away("abcde", "abc")  # should return False
 is_one_away("abc", "abcde")  # should return False
 is_one_away("abc", "bcc")  # should return False
+
+def test():
+    assert is_one_away("abcde", "abcd") == True
+    assert is_one_away("abde", "abcde") == True
+    assert is_one_away("a", "a") == True
+    assert is_one_away("abcdef", "abqdef") == True
+    assert is_one_away("abcdef", "abccef") == True
+    assert is_one_away("abcdef", "abcde") == True
+    assert is_one_away("aaa", "abc") == False
+    assert is_one_away("abcde", "abc") == False
+    assert is_one_away("abc", "abcde") ==False
+    assert is_one_away("abc", "bcc") == False
