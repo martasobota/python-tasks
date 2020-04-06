a10fafefcd3f3949781b03a0d737fae77e00911f
Marta Sobota <martazaryn@gmail.com>
2020-04-06 18:57:44 +0200

    Move zeros task

---
+++ leetcode/move_zeros.py
@@ -0,0 +1,24 @@
+from typing import List
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Example:
+
+# Input: [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Note:
+
+# You must do this in-place without making a copy of the array.
+# Minimize the total number of operations.
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pointer = 0
+        counter = 0
+        while counter < len(nums):
+            if nums[pointer] == 0: 
+                nums.append(nums.pop(pointer))
+            else: pointer += 1
+            counter += 1
